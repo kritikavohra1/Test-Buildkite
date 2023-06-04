9f05634a7768c931cf1c5417622e7fdf2341c678
Kritika Vohra <kvohra@seekasia.com>
2023-06-04 14:35:59 +0530

    adding env var

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+import {uiPage} from "../page-objects/ui-page"
+
+describe("Test page object model sample demo", () => {
+
+    beforeEach(()=>{
+
+        cy.visit('https://way2automation.com/way2auto_jquery/index.php')
+        cy.url().should('include', 'way2auto_jquery')
+    })
+
+    it('Enter details on page test', ()=>{
+        uiPage.enterDetails()
+    })
+})
